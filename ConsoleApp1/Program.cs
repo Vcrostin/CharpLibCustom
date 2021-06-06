7b5c798b9eb0fb1dc16bb4327c49b18a9ddb6f8e
Широкин Костя <kostyash2001@mail.ru>
2021-06-06 18:39:14 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,16 @@
+﻿using LibCustom.Request;
+using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            MailSend mailSend = new("qqwwzoopark@gmail.com", "smtp.gmail.com", "kostyash");
+            _ = mailSend.AttachMail("test-hackaton@yandex.ru", "ТЕСТ", "ТЕСТ");
+            mailSend.SendMail();
+            return 0;
+        }
+    }
+}
